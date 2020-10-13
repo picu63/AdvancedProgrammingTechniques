b4513afa4ed947f32e96c83c6f924bf65c8829cd
Piotr Olearczyk <picu63@gmail.com>
2020-10-13 20:14:35 +0200

    Add project files.

---
+++ ZTP.Scheduler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace ZTP.Scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Order> orders = CsvHelper("filepath");
+            SendEmail(orders);
+        }
+    }
+}
