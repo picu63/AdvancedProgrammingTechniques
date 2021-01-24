f306fe066afaca529b7ea019303c5f46ca40db4e
Adam Nowok <adam500.pl@gmail.com>
2021-01-24 14:46:54 +0100

    Added https

---
+++ Server/Controllers/ServerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServerController : ControllerBase
+    {
+
+        private readonly ILogger<ServerController> _logger;
+
+        public ServerController(ILogger<ServerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            string response = "I'm Server";
+            using (var wb = new WebClient())
+            {
+                var requestResponse = wb.DownloadString("http://localhost:62773/User");
+                return $"{response}, {requestResponse}";
+            }
+        }
+    }
+}
---
+++ User/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace User.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "I'm User";
+        }
+    }
+}
