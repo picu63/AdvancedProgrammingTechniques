b63f82ce8d78aff36e7be7163871fec6e3ca80af
Adam Nowok <adam500.pl@gmail.com>
2021-01-24 15:00:41 +0100

    changed controller

--- User/Controllers/UserController.cs
+++ User/Controllers/UserController.cs
@@ -12,11 +12,6 @@ namespace User.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<UserController> _logger;
 
         public UserController(ILogger<UserController> logger)
